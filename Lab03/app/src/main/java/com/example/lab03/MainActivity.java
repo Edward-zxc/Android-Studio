b0a948c5a4c8e4d0f5f0acc299800e7e3fa06676
Edward xc <3313338649@qq.com>
2024-05-21 19:59:55 +0800

    Update for lab03

--- Lab03/app/src/main/java/com/example/lab03/MainActivity.java
+++ Lab03/app/src/main/java/com/example/lab03/MainActivity.java
@@ -12,5 +12,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+// MainActivity.java
 public class MainActivity extends AppCompatActivity {
+    private TextView contactInfo;
+    private Button newContactButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        contactInfo = findViewById(R.id.contact_info);
+        newContactButton = findViewById(R.id.new_contact_button);
+
+        newContactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            String info = data.getStringExtra("info");
+            contactInfo.setText(info);
+            newContactButton.setText("联系人信息");
+        }
+    }
 }
--- Lab03/app/src/main/java/com/example/lab03/MainActivity2.java
+++ Lab03/app/src/main/java/com/example/lab03/MainActivity2.java
@@ -3,13 +3,40 @@ package com.example.lab03;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
 
 public class MainActivity2 extends AppCompatActivity {
+    private EditText nameInput;
+    private EditText companyInput;
+    private EditText phoneInput;
+    private EditText emailInput;
+    private Button doneButton;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        nameInput = findViewById(R.id.name_input);
+        companyInput = findViewById(R.id.company_input);
+        phoneInput = findViewById(R.id.phone_input);
+        emailInput = findViewById(R.id.email_input);
+        doneButton = findViewById(R.id.done_button);
+
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String info = nameInput.getText().toString() + "\n" +
+                        companyInput.getText().toString() + "\n" +
+                        phoneInput.getText().toString() + "\n" +
+                        emailInput.getText().toString();
+                Intent intent = new Intent();
+                intent.putExtra("info", info);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
 }
